aeaa3b2f6b8de6b07cacca0c3e6952977bb212ca
R-zin <razinm24bcs24@iiitkottayam.ac.in>
2026-06-19 15:08:51 +0530

    add: model for sql db

--- services/dbmodel.py
+++ services/dbmodel.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column,Integer,Text
+from sqlalchemy import Column, Integer, Text, String, ARRAY
 from database import Base
 
 class SpotifyAdmin(Base):
@@ -6,3 +6,11 @@ class SpotifyAdmin(Base):
     id = Column(Integer,primary_key=True)
     refresh_token = Column(Text,nullable=False)
     spotify_user_id = Column(Text,nullable=False)
+class SpotifyPlaylist(Base):
+    __tablename__ = "spotify_playlist"
+    id = Column(String,primary_key=True)
+    playlist_name = Column(String,nullable=False)
+    trackno = Column(Integer,nullable=False)
+    playlist_link = Column(String,nullable=False)
+    song_list = Column(ARRAY(Text),nullable=False)
+
