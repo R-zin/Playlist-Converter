c1ce67455127ea5c5d5b992e88df6ddac2a3b2d4
R-zin <razinm24bcs24@iiitkottayam.ac.in>
2026-06-20 21:02:00 +0530

    added db integration (possible wrong type for song_list)

--- routes/Converter.py
+++ routes/Converter.py
@@ -2,11 +2,14 @@ from fastapi import APIRouter,HTTPException,Header
 from services.Spotify_Service import Spotify
 #from ..services.logger import log_playlist
 from services.AppleParser import AppleParser
+from services.database import SessionLocal
+from services.dbmodel import SpotifyPlaylist
 import asyncio
 
 spotify = Spotify()
 appleParser = AppleParser()
 router = APIRouter()
+db = SessionLocal()
 
 @router.post("/convert/apple-music-to-spotify")
 async def convert(apple_music_playlist_url:str,playlist_name:str,authorization:str = Header()):
@@ -15,8 +18,10 @@ async def convert(apple_music_playlist_url:str,playlist_name:str,authorization:s
         if not apple_playlist:
             raise Exception
         res = await spotify.create_playlist(apple_playlist.songs)
-
-        await log_playlist(apple_music_playlist_url,res)
+        entry = SpotifyPlaylist(playlist_name=playlist_name,trackno=len(apple_playlist.songs),playlist_link=res,song_list=apple_playlist.songs)
+        db.add(entry)
+        db.commit()
+        db.refresh(entry)
         return {
             "playlist_url":res
         }
