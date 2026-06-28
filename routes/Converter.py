d1f9288c2617cab894f10fcea3771b22b2df3c6f
R-zin <razinm24bcs24@iiitkottayam.ac.in>
2026-06-28 22:24:52 +0530

    minor

--- routes/Converter.py
+++ routes/Converter.py
@@ -2,15 +2,15 @@ from fastapi import APIRouter,HTTPException,Header
 from services.Spotify_Service import Spotify
 #from ..services.logger import log_playlist
 from services.AppleParser import AppleParser
-#from services.database import SessionLocal
-#from services.dbmodel import SpotifyPlaylist
+from services.database import SessionLocal
+from services.dbmodel import SpotifyPlaylist
 from services.redis_file import client
 import asyncio
 
 spotify = Spotify()
 appleParser = AppleParser()
 router = APIRouter()
-#db = SessionLocal()
+db = SessionLocal()
 
 @router.post("/convert/apple-music-to-spotify")
 async def convert(apple_music_playlist_url:str,playlist_name:str,authorization:str = Header()):
@@ -19,10 +19,10 @@ async def convert(apple_music_playlist_url:str,playlist_name:str,authorization:s
         if not apple_playlist:
             raise Exception
         res = await spotify.create_playlist(apple_playlist.songs)
-        #entry = SpotifyPlaylist(playlist_name=playlist_name,trackno=len(apple_playlist.songs),playlist_link=res,song_list=apple_playlist.songs)
-        #db.add(entry)
-        #db.commit()
-        #db.refresh(entry)
+        entry = SpotifyPlaylist(playlist_name=playlist_name,trackno=len(apple_playlist.songs),playlist_link=res,song_list=apple_playlist.songs)
+        db.add(entry)
+        db.commit()
+        db.refresh(entry)
         return {
             "playlist_url":res
         }
