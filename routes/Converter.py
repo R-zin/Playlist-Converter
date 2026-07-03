cc175c0a52f2366c5583c61f3b8d46f16434fd39
R-zin <razinm24bcs24@iiitkottayam.ac.in>
2026-07-03 22:42:01 +0530

    attempt minor fix

--- routes/Converter.py
+++ routes/Converter.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter,HTTPException,Header
 from sqlalchemy import null
 
+from routes.Authentication import CLIENT_ID,CLIENT_SECRET
 from services.Spotify_Service import Spotify
 #from ..services.logger import log_playlist
 from services.AppleParser import AppleParser
 from services.database import SessionLocal
 from services.dbmodel import SpotifyPlaylist
 import httpx
+import base64
 from services.redis_file import client
 from datetime import datetime, timedelta
 from services.dbmodel import SpotifyAdmin
@@ -16,19 +18,27 @@ spotify = Spotify()
 appleParser = AppleParser()
 router = APIRouter()
 db = SessionLocal()
+st = CLIENT_ID + ':' + CLIENT_SECRET
+auth_head = base64.b64encode(st.encode()).decode()
 
 async def get_new_access_token(refresh_token:str):
-    async with httpx.AsyncClient() as client:
-        res = await client.post(
-            "https://accounts.spotify.com/api/token",
-            headers={"Content-Type": "application/x-www-form-urlencoded"},
-            data={"grant_type": "refresh_token",
-                  "refresh_token": refresh_token,}
-        )
-        if res.status_code == 200:
-            return res.json()["access_token"]
-        else:
-            raise HTTPException(status_code=401,detail="Invalid refresh token")
+
+    try:
+        async with httpx.AsyncClient() as client:
+            res = await client.post(
+                "https://accounts.spotify.com/api/token",
+                headers={"Content-Type": "application/x-www-form-urlencoded",
+                         "Authorization": f"Bearer {auth_head}"},
+                data={"grant_type": "refresh_token",
+                      "refresh_token": refresh_token, }
+            )
+            if res.status_code == 200:
+                return res.json()["access_token"]
+    except Exception as e:
+        raise HTTPException(status_code=404,detail=e)
+
+
+
 
 
 
@@ -51,7 +61,7 @@ async def convert(apple_music_playlist_url:str,playlist_name:str,description:str
             db.commit()
         else:
             token = admin.access_token
-        res = await spotify.create_playlist(token=token,playlist_name=playlist_name,songs=apple_playlist,description=description)
+        res = await spotify.create_playlist(token=str(token),playlist_name=playlist_name,songs=apple_playlist,description=description)
         entry = SpotifyPlaylist(playlist_name=playlist_name,trackno=len(apple_playlist.songs),playlist_link=res,song_list=apple_playlist.songs)
         db.add(entry)
         db.commit()
