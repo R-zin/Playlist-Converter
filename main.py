ce6f4875cf0a022ca9c101da57c5df947dd29402
R-zin <razinm24bcs24@iiitkottayam.ac.in>
2026-07-24 14:57:54 +0530

    fix:minor

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 from dotenv import load_dotenv
-
 load_dotenv(verbose=True)
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from routes.Authentication import router as auth_router
 from routes.Converter import router as converter
-import os
-
 
 app = FastAPI(title="Song Parser")
-
-test = os.getenv("REDIRECT_URL")
-if not test:
-    print("Redirect URL not set")
-
 
 app.include_router(auth_router)
 app.include_router(converter)
